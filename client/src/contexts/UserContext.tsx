b38f22933d9a7a16e695c5bf7a26a79a66851c44
= <7rubengarcia7@gmail.com>
2020-09-24 08:05:30 -0700

    [client]: added a movie context

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,13 +1,20 @@
 import React, { createContext, useState } from "react";
 
 interface UserState {
+  id: number;
+  setId?: React.Dispatch<React.SetStateAction<number>>;
   isLoggedIn: boolean;
+  setIsLoggedIn?: React.Dispatch<React.SetStateAction<boolean>>;
   email: string;
+  setEmail?: React.Dispatch<React.SetStateAction<string>>;
   createdAt: string;
+  setCreatedAt?: React.Dispatch<React.SetStateAction<string>>;
   updatedAt: string;
+  setUpdatedAt?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const initialState: UserState = {
+  id: 0,
   isLoggedIn: false,
   email: "",
   createdAt: "",
@@ -21,6 +28,7 @@ interface Props {
 }
 
 function UserProvider({ children }: Props) {
+  const [id, setId] = useState<number>(0);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [createdAt, setCreatedAt] = useState<string>("");
@@ -28,18 +36,18 @@ function UserProvider({ children }: Props) {
 
   return (
     <UserContext.Provider
-      value={
-        {
-          isLoggedIn,
-          setIsLoggedIn,
-          email,
-          setEmail,
-          createdAt,
-          setCreatedAt,
-          updatedAt,
-          setUpdatedAt,
-        } as any
-      }
+      value={{
+        id,
+        setId,
+        isLoggedIn,
+        setIsLoggedIn,
+        email,
+        setEmail,
+        createdAt,
+        setCreatedAt,
+        updatedAt,
+        setUpdatedAt,
+      }}
     >
       {children}
     </UserContext.Provider>
